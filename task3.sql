cf05825291a345fd10e460b001c7b55bd3850050
ministr-kaifa <eyzeep@yandex.ru>
2023-12-14 00:22:48 +0300

    review fixes

--- task3.sql
+++ task3.sql
@@ -6,8 +6,22 @@ SELECT
   avg(v.compensation_from)                                AS avg_compensation_from,
   avg(v.compensation_to)                                  AS avg_compensation_to,
   avg(v.compensation_from) + avg(v.compensation_to) / 2.0 AS avg_compensation
-FROM
-  vacancy v
+FROM (
+  SELECT
+    CASE
+      WHEN compensation_gross IS TRUE
+        THEN compensation_to * 0.87
+      ELSE compensation_to
+      END AS compensation_to,
+    CASE
+      WHEN compensation_gross IS TRUE
+        THEN compensation_from * 0.87
+      ELSE compensation_from
+      END AS compensation_from,
+    employer_id
+  FROM
+    vacancy
+) v
 LEFT JOIN (
   SELECT
     e.id      AS employer_id,
@@ -23,3 +37,5 @@ LEFT JOIN (
 ON
   e.employer_id = v.employer_id
 GROUP BY e.area_id, e.area_name;
+
+
--- task4.sql
+++ task4.sql
@@ -2,30 +2,31 @@
 --    и месяца с наибольшим количеством резюме
 
 SELECT
-  month_number,
-  vacancy_amount,
-  resume_amount
-FROM
-  generate_series(1, 12) AS month_number
-LEFT JOIN (
+  COALESCE(v.year, r.year) AS year,
+  COALESCE(v.month_number, r.month_number) AS month_number,
+  COALESCE(v.vacancy_amount, NULL) AS vac_amount,
+  COALESCE(r.resume_amount, NULL) AS res_amount
+FROM (
   SELECT
-    extract(month FROM created_at) AS vacancy_month_number,
-    count(*) AS vacancy_amount
+    extract(month FROM created_at)  AS month_number,
+    extract(year  FROM created_at)  AS year,
+    count(*)                        AS vacancy_amount
   FROM
     vacancy
-  GROUP BY vacancy_month_number
-  ORDER BY vacancy_amount 
+  GROUP BY year, month_number
+  ORDER BY vacancy_amount
   LIMIT 1
-) AS v ON vacancy_month_number = month_number
-LEFT JOIN (
+) AS v 
+FULL JOIN (
   SELECT
-    extract(month FROM created_at) AS resume_month_number,
-    count(*) AS resume_amount
+    extract(month FROM created_at)  AS month_number,
+    extract(year  FROM created_at)  AS year,
+    count(*)                        AS resume_amount
   FROM
     resume
-  GROUP BY resume_month_number
-  ORDER BY resume_amount 
+  GROUP BY year, month_number
+  ORDER BY resume_amount
   LIMIT 1
-) AS r ON resume_month_number = month_number
+) AS r ON r.month_number = v.month_number AND v.year = r.year
 WHERE
   v.vacancy_amount IS NOT NULL OR r.resume_amount IS NOT NULL;
